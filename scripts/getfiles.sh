9acb95fa8cbba3e751888bc5a278e3f02e744923
marcusbooyah <booyah@uw.edu>
2015-08-08 21:10:42 -0700

    testing specified directory

--- scripts/getfiles.sh
+++ scripts/getfiles.sh
@@ -5,7 +5,7 @@ if [[ $TRAVIS_PULL_REQUEST != "false" ]]; then
   cat changes.out | grep -o 'tutorials/.*\.ipynb$' | cut -f2- -d'/' > notebooks.out
 else
   echo "This is not a pull request, all files will be tested."
-  find ../ ! -path "../TestIPy/miniconda/*" -name "*.ipynb" -print > notebooks.out
+  find ../$NOTEBOOKS ! -path "../TestIPy/miniconda/*" -name "*.ipynb" -print > notebooks.out
 fi
 echo "The following files will be tested:"
 cat notebooks.out
