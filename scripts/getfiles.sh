3488489881aaec0fe6f95571afdced65aa29abd6
marcusbooyah <booyah@uw.edu>
2015-08-09 11:48:01 -0700

    testing TestIPy

--- scripts/getfiles.sh
+++ scripts/getfiles.sh
@@ -2,10 +2,10 @@ rm -f notebooks.out
 if [[ $TRAVIS_PULL_REQUEST != "false" ]]; then
   echo "This is a pull request, only changed files will be tested."
 	git diff --name-only $TRAVIS_BRANCH HEAD > changes.out
-  cat changes.out | grep -o 'tutorials/.*\.ipynb$' | cut -f2- -d'/' > notebooks.out
+  cat changes.out | grep -o '.*\.ipynb$' > notebooks.out
 else
-  echo "This is not a pull request, all files will be tested."
-  find . ! -path "../TestIPy/miniconda/*" -name "*.ipynb" -and -path "../TestIPy/$NOTEBOOKS" -print > notebooks.out
+  echo "This is not a pull request, all files in specified directory will be tested."
+  find "../$NOTEBOOKS" ! -path "../scripts/miniconda/*" -name "*.ipynb" -print | cut -c 4- > notebooks.out
 fi
 echo "The following files will be tested:"
 cat notebooks.out
