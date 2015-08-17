3153fd7a4a9d94ab4e20999b06f4400b516ea82c
marcusbooyah <booyah@uw.edu>
2015-08-17 13:56:53 -0400

    add s3 test

--- scripts/runipy.sh
+++ scripts/runipy.sh
@@ -4,12 +4,15 @@ while read line
         do
           	echo "Processing $line"
                 if runipy "./$line" "./$line-tested.ipynb"; then # 2>"./$line.out"; then
-                                echo "$line" passed.
+                        echo "$line" passed.
                         echo
                 else
-                                echo "$line" failed.
-                        ERROR=1
+                        echo "$line" failed.
+                        cp ./$line-tested.ipynb ./fails
+			ERROR=1
                         echo
                 fi
         done < notebooks.out
+echo "These notebooks failed"
+ls -l ./fails
 exit $ERROR
