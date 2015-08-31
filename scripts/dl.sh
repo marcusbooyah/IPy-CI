f5cac19caf3214384aec103c5d9f68e578364111
marcusbooyah <booyah@uw.edu>
2015-08-31 14:04:17 -0700

    refactor

---
+++ scripts/dl.sh
@@ -0,0 +1,5 @@
+set -e
+
+wget https://raw.githubusercontent.com/marcusbooyah/IPy-CI/master/scripts/prepare.sh -P ./scripts
+wget https://raw.githubusercontent.com/marcusbooyah/IPy-CI/master/scripts/getfiles.sh -P ./scripts
+wget https://raw.githubusercontent.com/marcusbooyah/IPy-CI/master/scripts/runipy.sh -P ./scripts
